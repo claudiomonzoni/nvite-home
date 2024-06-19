6366a66aab9938d037b2cf878edc9de960537b8e
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-06-19 10:26:58 -0600

    cambio de branch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import react from "@astrojs/react";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://www.nvite.me',
-    integrations: [sitemap()],
+  site: 'https://www.nvite.me',
+  integrations: [sitemap(), react(), mdx()]
 });
---
+++ src/components/bodas/Hero.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import gsap from "gsap";
+import Style from "../../estilos/bodas/hero.module.scss";
+import invitadosData from "../../pages/bodas/data/invitados.json";
+
+export default function Hero({ nombres, fecha }) {
+  const [invitado, setInvitado] = useState("ID inexistente");
+  const [pase, setPase] = useState(0);
+  useEffect(() => {
+    document.querySelector(".contenido").classList.remove("opa");
+    // confirmacion de id
+    const valores = window.location.search;
+    const params = new URLSearchParams(valores);
+    const id = params.get("id");
+    // animacion intro
+    if ( id && id < invitadosData.length) {
+      setInvitado(invitadosData[id].nombre);
+      setPase(invitadosData[id].pases);
+    }
+    const tl = gsap.timeline();
+    gsap.from(".contenido", { opacity: 0, y: -30, duration: 1, delay: 0.2 });
+    tl.from("#bande", {
+      opacity: 0,
+      y: -30,
+      delay: 2,
+      height: 500,
+      duration: 1,
+      ease: "power4.out",
+    });
+    tl.from("#centro *", {
+      opacity: 0,
+      y: -30,
+      duration: 1,
+      ease: "power4.out",
+      stagger: { amount: 0.5 },
+    });
+  }, []);
+  return (
+    <>
+      <section id={Style["hero"]} className="grid contenido opa">
+        <div className={Style.bandeja} id="bande">
+          <div className={Style.centro} id="centro">
+            <span className={Style.familia} id="invitado">
+              {invitado}
+            </span>
+            <h1 dangerouslySetInnerHTML={{ __html: nombres }}></h1>
+            <p>
+              desean invitarte a <b>celebrar su boda</b>
+            </p>
+            <p className={Style.fecha}>{fecha}</p>
+          </div>
+          <div id={Style["pases"]}>
+            No. de pases: <span id="NumeroPases">{pase}</span>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
