a4dd56048042ec483f0bbea2bd6c4655e018b2f6
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-06-19 22:11:31 -0600

    resolviendo sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nvite.me',
-  integrations: [sitemap(), react(), mdx()]
+  integrations: [sitemap(), react(), mdx()],
+  output: "server",
+  adapter: vercel({
+    imageService: true,
+  })
 });
