ef374334c436b9c90d4b5b77375a61add7d78371
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-09-07 16:01:44 -0600

    avance

--- src/components/bodas/Confirmacion.jsx
+++ src/components/bodas/Confirmacion.jsx
@@ -107,9 +107,10 @@ export default function Confirmacion({ whatsapp, dias_antes, version }) {
 
               <label for="comentarios">Envíanos algún saludo (opcional):</label>
               <textarea name="comentarios" id="comentarios"></textarea>
-              <a href="#" className="btn desactivado" id="btnconfirmar">
+              <a href="#" className="btn redondo  desactivado" id="btnconfirmar">
                 Confirmar mi asistencia
               </a>
+              
             </form>
           </div>
         </div>
