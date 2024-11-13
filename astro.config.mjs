e203635e6a224b6856b6b3af50202a00ea47f661
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-11-13 11:01:54 -0600

    quitando react de config para pruebas

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from '@astrojs/sitemap';
-import react from "@astrojs/react";
+// import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 import icon from "astro-icon";
 
@@ -10,7 +10,12 @@ import db from "@astrojs/db";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.nvita.me',
-  integrations: [sitemap(), react(), mdx(), icon(), db()],
+  integrations: [sitemap()
+    // , react()
+    , mdx()
+    , icon()
+    , db()],
+
   output: "server",
   adapter: vercel({
     imageService: true
