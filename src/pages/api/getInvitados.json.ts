e682dd7dc4de0d77013bef514f6fb5413190efe4
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-11-14 11:24:09 -0600

    ajustes, a ver si ahora si jala

--- src/pages/api/getInvitados.json.ts
+++ src/pages/api/getInvitados.json.ts
@@ -2,38 +2,44 @@ import type { APIRoute } from "astro";
 import { Invitados, Usuario, db, eq, desc } from "astro:db";
 import { NvitaAuth } from "../../firebase/config";
 
-
-export const GET: APIRoute = async () => {
+export const GET: APIRoute = async ({ request }) => {
   try {
-    const usuarioEmail = NvitaAuth.currentUser?.email;
-    if(!usuarioEmail){
-      return new Response(
-        JSON.stringify({message: "usuario indefinido"}),
-        {status: 401}
-      )
+    const url = new URL(request.url);
+    const usuarioEmail = url.searchParams.get("email");
+
+    if (!usuarioEmail) {
+      return new Response(JSON.stringify({ message: "usuario indefinido" }), {
+        status: 401,
+      });
     }
     //aqui hay que hacer el filtro por usuario
-    const user = await db.select().from(Usuario).where(eq(Usuario.email, usuarioEmail ));
+    const user = await db
+      .select()
+      .from(Usuario)
+      .where(eq(Usuario.email, usuarioEmail));
     // Verificar que el usuario exista y tenga un ID válido
     if (!user.length || !user[0].id) {
       return new Response(
-        JSON.stringify({ message: "Usuario no encontrado o sin ID válido" }), 
+        JSON.stringify({ message: "Usuario no encontrado o sin ID válido" }),
         { status: 404 }
       );
     }
     // Consulta para obtener los invitados del usuario
     const data = await db
-    .select()
-    .from(Invitados)
-    .orderBy(desc(Invitados.id))
-    .where(eq(Invitados.usuarioId, user[0].id )); 
+      .select()
+      .from(Invitados)
+      .orderBy(desc(Invitados.id))
+      .where(eq(Invitados.usuarioId, user[0].id));
     return new Response(JSON.stringify(data), {
       status: 200,
     });
   } catch (error) {
     return new Response(
       JSON.stringify({
-        message: error instanceof Error ? error.message : "Ocurrio un error al obtener los invitados",
+        message:
+          error instanceof Error
+            ? error.message
+            : "Ocurrio un error al obtener los invitados",
       })
     );
   }
