f72fee0a40dc3f777bf87a9abee193239419388c
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-11-14 10:25:57 -0600

    avance

--- src/pages/api/getInvitados.json.ts
+++ src/pages/api/getInvitados.json.ts
@@ -1,13 +1,27 @@
 import type { APIRoute } from "astro";
 import { Invitados, Usuario, db, eq, desc } from "astro:db";
 import { NvitaAuth } from "../../firebase/config";
-const usuarioEmail = NvitaAuth.currentUser?.email;
 
 
 export const GET: APIRoute = async () => {
   try {
+    const usuarioEmail = NvitaAuth.currentUser?.email;
+    if(!usuarioEmail){
+      return new Response(
+        JSON.stringify({message: "usuario indefinido"}),
+        {status: 401}
+      )
+    }
     //aqui hay que hacer el filtro por usuario
     const user = await db.select().from(Usuario).where(eq(Usuario.email, usuarioEmail ));
+    // Verificar que el usuario exista y tenga un ID válido
+    if (!user.length || !user[0].id) {
+      return new Response(
+        JSON.stringify({ message: "Usuario no encontrado o sin ID válido" }), 
+        { status: 404 }
+      );
+    }
+    // Consulta para obtener los invitados del usuario
     const data = await db
     .select()
     .from(Invitados)
