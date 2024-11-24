1756674df61cfe645bea200bc34f02dee630e3be
claudiomonzoni <claudiomonzoni@hotmail.com>
2024-11-24 14:14:11 -0600

    afuerzas

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ export const onRequest = defineMiddleware(async (context, next)  => {
     )){
         return context.redirect('/panel')
     }
-    if(!estaDentro && pathname !== "/"  && pathname !== "/api/getInvitados.json" && pathname !== "/panel/ingresar" && pathname !== "/panel/registro" && context.request.method === "GET"){
+    if(!estaDentro && pathname !== "/" && pathname !== "/bodas/" && pathname !== "/api/getInvitados.json" && pathname !== "/panel/ingresar" && pathname !== "/panel/registro" && context.request.method === "GET"){
         return context.redirect('/panel/ingresar')
     }
     return next()
