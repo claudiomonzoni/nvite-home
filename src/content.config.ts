23576fdcd4a183d18afb561bb8ab287753b6e508
claudiomonzoni <claudiomonzoni@hotmail.com>
2025-01-03 17:41:00 -0600

    quitando la imagen

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,17 +8,19 @@ import { glob, file } from 'astro/loaders';
 const bodas = defineCollection({
     // type: "content", // v2.5.0 and later
     loader: glob({ pattern: "**/*.mdx", base: "./src/content/bodas" }),
-    schema: ({ image }) => z.object({
+    schema:
+    //  ({ image }) =>
+       z.object({
       titulo: z
         .string()
         .max(
           80,
           "Para un mejor Seo, por favor ingrese un titulo de menos de 80 caracteres"
         ),
-      cover: image().refine((img) => img.width >= 200, {
-        message:
-          "¡La imagen de portada debe tener al menos 200 píxeles de ancho!",
-      }), 
+      // cover: image().refine((img) => img.width >= 200, {
+      //   message:
+      //     "¡La imagen de portada debe tener al menos 200 píxeles de ancho!",
+      // }), 
       extracto: z.string(),
       descripcion: z.string(),
       whatsapp: z.number(),
@@ -53,65 +55,65 @@ const bodas = defineCollection({
   });
 
 
-const quince = defineCollection({
-    // type: "content", // v2.5.0 and later
-    loader: glob({ pattern: "**/*.mdx", base: "./src/content/quince" }),
-    schema: z.object({
-        titulo: z
-          .string()
-          .max(
-            80,
-            "Para un mejor Seo, por favor ingrese un titulo de menos de 80 caracteres"
-          ),
+// const quince = defineCollection({
+//     // type: "content", // v2.5.0 and later
+//     loader: glob({ pattern: "**/*.mdx", base: "./src/content/quince" }),
+//     schema: z.object({
+//         titulo: z
+//           .string()
+//           .max(
+//             80,
+//             "Para un mejor Seo, por favor ingrese un titulo de menos de 80 caracteres"
+//           ),
   
-        // cover: image().refine((img) => img.width >= 200, {
-        //   message:
-        //     "¡La imagen de portada de quince debe tener al menos 700 píxeles de ancho!",
-        // }).optional(),
-        coverAlt: z.string(),
-        extracto: z.string(),
-        descripcion: z.string(),
-        whatsapp: z.number(),
-        quinceanera: z.string(),
-        fecha: z.date(),
-        consideraciones: z.array(z.string()).optional(),
-        vestimenta: z.string(),
-        frase_amor: z.string(),
-        ceremonia: z.object({
-          horario: z.string(),
-          direccion: z.string(),
-          lat: z.number(),
-          lng: z.number(),
-        }).optional(),
-        recepcion: z.object({
-          horario: z.string().optional(),
-          direccion: z.string().optional(),
-          lat: z.number().optional(),
-          lng: z.number().optional(),
-        }).optional(),
-        itinerario: z.array(
-          z.object({
-            titulo: z.string(),
-            lugar: z.string(),
-            hora: z.number(),
-          })
-        ).optional(),
-        beneficiario: z.string().optional(),
-        banco: z.string().optional(),
-        cuenta: z.number().optional(),
-      }),
-  });
+//         // cover: image().refine((img) => img.width >= 200, {
+//         //   message:
+//         //     "¡La imagen de portada de quince debe tener al menos 700 píxeles de ancho!",
+//         // }).optional(),
+//         coverAlt: z.string(),
+//         extracto: z.string(),
+//         descripcion: z.string(),
+//         whatsapp: z.number(),
+//         quinceanera: z.string(),
+//         fecha: z.date(),
+//         consideraciones: z.array(z.string()).optional(),
+//         vestimenta: z.string(),
+//         frase_amor: z.string(),
+//         ceremonia: z.object({
+//           horario: z.string(),
+//           direccion: z.string(),
+//           lat: z.number(),
+//           lng: z.number(),
+//         }).optional(),
+//         recepcion: z.object({
+//           horario: z.string().optional(),
+//           direccion: z.string().optional(),
+//           lat: z.number().optional(),
+//           lng: z.number().optional(),
+//         }).optional(),
+//         itinerario: z.array(
+//           z.object({
+//             titulo: z.string(),
+//             lugar: z.string(),
+//             hora: z.number(),
+//           })
+//         ).optional(),
+//         beneficiario: z.string().optional(),
+//         banco: z.string().optional(),
+//         cuenta: z.number().optional(),
+//       }),
+//   });
 
-  const imagenes = defineCollection({
-    schema: ({ image }) =>
-      z.object({
-        title: z.string(),
-        cover: image().refine((img) => img.width >= 1080, {
-          message: "Cover image must be at least 1080 pixels wide!",
-        }),
-        Alt: z.string(),
-      }),
-  });
+//   const imagenes = defineCollection({
+//     schema: ({ image }) =>
+//       z.object({
+//         title: z.string(),
+//         cover: image().refine((img) => img.width >= 1080, {
+//           message: "Cover image must be at least 1080 pixels wide!",
+//         }),
+//         Alt: z.string(),
+//       }),
+//   });
 
 // 4. Export a single `collections` object to register your collection(s)
-export const collections = { bodas, quince, imagenes };
+export const collections = { bodas };
