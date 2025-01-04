cd8f716d0fef8ac9910e94a99d03d7d30c3305b5
claudiomonzoni <claudiomonzoni@hotmail.com>
2025-01-04 15:11:27 -0600

    limpie la carpeta de vercel

--- .vercel/output/static/_astro/confirmacion_module.764fe6db.CeT17MpT.js
+++
@@ -1,1 +0,0 @@
-const n="_anime_2xxdn_1",a="_sombra_2xxdn_13",o="_confirmacion_2xxdn_14",_="_bandeja_2xxdn_31",i={anime:n,sombra:a,confirmacion:o,bandeja:_,"slide-in":"_slide-in_2xxdn_1"};export{i as e};
