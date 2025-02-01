d7532799bf1691c3efcacdf16f6183bf9c7a139f
claudiomonzoni <claudiomonzoni@hotmail.com>
2025-02-01 10:43:54 -0600

    quitando el middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,58 +2,7 @@ import { defineMiddleware } from "astro:middleware";
 import { NvitaAuth } from "./firebase/config";
 
 export const onRequest = defineMiddleware(async (context, next) => {
-    const estaDentro = NvitaAuth.currentUser; // Verifica si el usuario está autenticado
-    const { pathname } = context.url;
-    const method = context.request.method;
-
-    // Rutas específicas que requieren autenticación para métodos sensibles
-    const rutasProtegidas = [
-        /^\/api\/addInvitados\.json$/, // Ruta para agregar invitados
-        /^\/api\/\d+\.json$/,          // Ruta para un invitado específico por ID
-    ];
-
-    const esRutaProtegida = rutasProtegidas.some((ruta) => ruta.test(pathname));
-
-    // Verificar si es una ruta protegida con métodos sensibles
-    if (esRutaProtegida && (method === "POST" || method === "DELETE" || method === "PATCH")) {
-        if (!estaDentro) {
-            return new Response(JSON.stringify({ message: "No autorizado" }), { status: 401 });
-        }
-        return next();
-    }
-    
-    if(estaDentro && (
-        pathname === '/panel/ingresar' ||
-        pathname === '/panel/registro'
-      
-    )){
-        return context.redirect('/panel')
-    }
-
-    const rutasPublicas = [
-        /^\/$/,                              // Página principal
-        /^\/bodas$/,                        
-        /^\/quince$/,                      
-        /^\/api\/getInvitados\.json$/,       // Ruta específica de la API
-        /^\/panel\/ingresar$/,               // Página de ingreso
-        /^\/panel\/registro$/,               // Página de registro
-        /^\/bodas(\/.*)?$/,                  // Cualquier subruta en /bodas
-        /^\/quince(\/.*)?$/,                 // Cualquier subruta en /quince
-        /^\/terminos-condiciones(\/.*)?$/,                 // Cualquier subruta en /quince
-        // /^\/api\/.*$/                        // Todas las rutas de la API con método GET
-    ];
-
-    const esRutaPublica = rutasPublicas.some((ruta) => ruta.test(pathname));
-
-    // Permitir acceso a rutas públicas o redirigir si no está autenticado
-    if (!estaDentro) {
-        if (esRutaPublica || (pathname.startsWith('/api/') )) {
-            return next();
-        } else {
-            // Redirigir a la página de ingreso si el usuario no está autenticado
-            return context.redirect('/panel/ingresar');
-        }
-    }
+  
 
     return next()
     
